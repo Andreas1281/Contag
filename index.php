a27259df9e7e4ad2db5ef126a906c2125a6e9159
Andreas Wessolly <andreaswessolly@Andreass-MBP.office-factory.net>
2015-04-08 13:29:41 +0200

    Navileiste im ausgeloggten Zustand angepasst

--- index.php
+++ index.php
@@ -19,10 +19,7 @@
   <?php session_start();
 	if (isset($_SESSION["id"])) {?>
 
- 
-
-
-  <!-- Main bar -->
+ <!-- Main bar -->
   <nav id="main_nav" class="cyan lighten-1" role="navigation">
     <div class="container">
       <div class="nav-wrapper"><a id="logo-container" onclick="load('index');" class="brand-logo">SHOPTAG</a>
@@ -62,29 +59,18 @@
   <!-- LOGGED OUT ################################################-->
   <?php } else {?>
 
-  <!-- User bar -->
-  <nav id="user" class="cyan darken-2" role="navigation">
-    <div class="container">
-      <div class="nav-wrapper"><a id="logo-container" href="#" class="brand-logo"></a>
-        <ul class="right">
-        
-          <li><a class="modal-trigger" href="#modal_register">Register</a></li>
-          <li><a class="modal-trigger" href="#modal_register">Login</a></li>
-
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-  <!-- Main bar -->
+ <!-- Main bar -->
   <nav id="main_nav" class="cyan lighten-1" role="navigation">
     <div class="container">
       <div class="nav-wrapper"><a id="logo-container" onclick="load('index');" class="brand-logo">SHOPTAG</a>
         <ul class="right">
+          <li><a class="modal-trigger" href="#modal_register">Register</a></li>
+          <li><a class="modal-trigger" href="#modal_register">Login</a></li>
           <li><a onclick="load('about');">About</a></li>
           <li><a onclick="load('news');">News</a></li>
-	  <li><a onclick="load('retrieve');">Check Code</a></li>
+          <li><a onclick="load('retrieve');">Check Code</a></li>
         </ul>
+
         <ul id="nav-mobile" class="side-nav">
           <li><a href="#">Navbar Link</a></li>
         </ul>
@@ -95,6 +81,16 @@
 
   <?php } ?> 
  <!-- ############################################################# -->
+
+
+
+
+
+
+
+
+
+
 
  <!-- Page content -->
  <div class="section no-pad-bot" id="index-banner">
