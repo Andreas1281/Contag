e1136aeb8092fe082a0b83deda62bbf037960449
Leon Krieg <lkrieg@static.93.57.47.78.clients.your-server.de>
2015-04-11 03:07:28 +0200

    Comments & small tweaks

--- php/db_handle.php
+++ php/db_handle.php
@@ -1,65 +1,86 @@
 <?php
+ 
+// Load DB object for all functions
+try { $db = new PDO("sqlite:../db/db.sqlite"); 
+    } catch(PDOException $e) die("DB error: ".$e);
 
-try { 	$db = new PDO("sqlite:../db/db.sqlite"); 
-    }   catch(PDOException $e) { exit("<h1>Datenbank-Fehler</h1>".$e); }
+// Function: 	Create new DB from template file
+// Call: 	db_init()
+function db_init(){
 
+	$db_template = file('../db/db_template.txt');
+	foreach ($db_template as $handle){
 
+        	$db = $GLOBALS["db"];
+        	$db->exec($handle);
+	}
+	return 0;
+}
+
+// Function: 	Create new DB address entry
+// Call : 	db_add("de_de", $array)
 function db_add($locale, $values) {
-
 
 	$keys = implode(",",array_keys($values));
 	$vals = implode(",",array_values($values));
 	$handle = "INSERT INTO '$locale' ($keys) VALUES ($vals)";
+
 	$db = $GLOBALS["db"];
-        $db_request = $db->prepare($handle);
-        $db_request->execute();
+   	$db_request = $db->prepare($handle);
+	try { $db_request->execute();
+    	} catch(PDOException $e) die("DB writing error: ".$e);
 	echo "Executed Query: ".$handle;
+	return 0;
 }
 
+// Function: 	Get all data for a certain address entry
+//		(DB needs better id system!)
+// Call: 	db_get("de_de",user_id,id)
+// Return:	Array
 function db_get($locale, $user_id, $id) {
 
+	$handle = "SELECT * FROM '$locale' 
+		   where user_id='$user_id' AND id='$id'";
+	
 	$db = $GLOBALS["db"];
-	$handle = "SELECT * FROM '$locale' where user_id='$user_id' AND id='$id'";
 	$db_request = $db->prepare($handle); 
 	$db_request->execute();
 	$db_output = $db_request->fetchAll(); 
-	return $db_output[0];
+	return $db_output[0]; // Array
 }
 
-$data = db_get("de_de",1,1);
-echo $data["last_name"];
+//Function:	Example calls
+//Called:	on init
+function main() {
 
-$add_query = array( 
-	user_id => "\"5\"",
-	type => "\"1\"",
-	organisation => "\"Testorga\"",
-	first_name => "\"Andreas\"",
-	last_name => "\"Wessolly\"",
-	title_1 => "\"Prof. Dr.\"",
-	description_1 => "\"c/o Office-Factory\"",
-	street_name => "\"Am Rosengarten\"",
-	street_number => "\"20\"" );
+	//db_init(): // Create new db if missing
+	echo db_get("de_de",1,1)["last_name"]; // String (Single Field)
+	print_r( db_get("de_de",1,1) ); // Array (All entry data)
 
-db_add("de_de",$add_query);
-
-
-
-
-
-
-
-
-function db_init()
-{
-	$db_template = file('../db/db_template.txt'); 
-	foreach ($db_template as $handle) 
-	{
-        	$db = $GLOBALS["db"];
-        	$db->exec($handle);
-	}
+	// Aw shit... phone number formatting, 
+	// TODO: Change field type
+	db_add("de_de", [
+		user_id => "\"5\"",
+		type => "\"1\"",
+		organisation => "\"Wessolly Mobile Marketing\"",
+		first_name => "\"Andreas\"",
+		last_name => "\"Wessolly\"",
+		title_1 => "\"Prof. Dr.\"",
+		title_2 => "\"Allgemeinmediziner\"",
+		description_1 => "\"c/o Office-Factory\"",
+		description_2 => "\"Erste Etage\"",
+		street_name => "\"Am Rosengarten\"",
+		street_number => "\"20\"",
+		postal_codel => "\"36037\"",
+		city => "\"Fulda\"",
+		region => "\"Hessen\"",
+		email_address => "\"email@address.com\"",
+		phone_number => "\"1234567\"",
+		mobile_number => "\"1234567\"",
+		fax_number => "\"1234568\""
+	]);
+	return 0;
 }
 
-db_init();
-
-
+main();
 ?>
